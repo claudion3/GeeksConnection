8d4fe2ee88f1bb77dc5c7f86f8207603c84a420c
claudion3 <claudion3@gmail.com>
2021-10-25 19:46:49 +0200

    Add validation to check if user exist in database

--- server/graphql/resolvers/users.js
+++ server/graphql/resolvers/users.js
@@ -1,12 +1,23 @@
 require('dotenv').config();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
 
 const User = require('../../models/User');
 
 module.exports = {
 	Mutation: {
 		async register(_, { registerInput: { username, email, password } }) {
+			//Make sure user doesn't already exist
+			const user = await User.findOne({ username });
+
+			if (user) {
+				throw new UserInputError('Username is taken', {
+					errors: {
+						username: 'This username is taken',
+					},
+				});
+			}
 			//Hash password and create an auth token
 			password = await bcrypt.hash(password, 12);
 
