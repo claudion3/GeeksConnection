d3f50b7289c2e150a517af2abf1bd9dd326323d1
claudion3 <claudion3@gmail.com>
2021-10-25 19:55:53 +0200

    Add more validation

--- server/graphql/resolvers/users.js
+++ server/graphql/resolvers/users.js
@@ -4,10 +4,24 @@ const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server');
 
 const User = require('../../models/User');
+const { validateRegisterInput } = require('../../utils/validators');
 
 module.exports = {
 	Mutation: {
-		async register(_, { registerInput: { username, email, password } }) {
+		async register(
+			_,
+			{ registerInput: { username, email, password, confirmPassword } },
+		) {
+			//Validation user data
+			const { valid, errors } = validateRegisterInput(
+				username,
+				email,
+				password,
+				confirmPassword,
+			);
+			if (!valid) {
+				throw new UserInputError('Errors', { errors });
+			}
 			//Make sure user doesn't already exist
 			const user = await User.findOne({ username });
 
