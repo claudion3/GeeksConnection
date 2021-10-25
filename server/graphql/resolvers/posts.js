36f34ed3dfce909770dbd34de112dcdbc6465542
claudion3 <claudion3@gmail.com>
2021-10-25 20:49:41 +0200

    Add get post by Id

--- server/graphql/resolvers/posts.js
+++ server/graphql/resolvers/posts.js
@@ -2,10 +2,24 @@ const Post = require('../../models/Post');
 
 module.exports = {
 	Query: {
+		//Get All Posts
 		async getPosts() {
 			try {
 				const posts = await Post.find();
 				return posts;
+			} catch (err) {
+				throw new Error(err);
+			}
+		},
+		//Get post by :id
+		async getPost(_, { postId }) {
+			try {
+				const post = await Post.findById(postId);
+				if (post) {
+					return post;
+				} else {
+					throw new Error('Post not found');
+				}
 			} catch (err) {
 				throw new Error(err);
 			}
