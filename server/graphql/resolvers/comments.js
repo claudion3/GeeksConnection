693a7f43de7562ac4f5afd7fe1ca4e5ff7c57f88
claudion3 <claudion3@gmail.com>
2021-10-26 09:33:44 +0200

    delete comment added

--- server/graphql/resolvers/comments.js
+++ server/graphql/resolvers/comments.js
@@ -25,5 +25,25 @@ module.exports = {
 				throw new UserInputError('Post not found');
 			}
 		},
+		async deleteComment(_, { postId, commentId }, context) {
+			const { username } = checkAuth(context);
+			const post = await Post.findById(postId);
+
+			if (post) {
+				const commentIndex = post.comments.findIndex(
+					comment => comment.id === commentId,
+				);
+
+				if (post.comments[commentIndex].username === username) {
+					post.comments.splice(commentIndex, 1);
+					await post.save();
+					return post;
+				} else {
+					throw new AuthenticationError('Action not allowed');
+				}
+			} else {
+				throw new UserInputError('Post not found');
+			}
+		},
 	},
 };
